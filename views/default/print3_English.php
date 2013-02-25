960d9bbced8a938b46aa166794b196cf1ca86c63
Mark <m@rkw.me>
2013-02-25 09:39:30 +0000

    [OE-2684] updated consent form 3 based on feedback from rob & mel

--- views/default/print3_English.php
+++ views/default/print3_English.php
@@ -2,35 +2,37 @@
 	h1,h2,h4 { display: inline; margin: 0 auto; width: 400px; text-align: center; }
 	.noth3 { font-size: 12pt; font-weight: normal; }
 	.spacer { height: 2em; }
+	table { border-spacing: 0; border-collapse: collapse; }
 	table.signature { margin-left: 0; }
-	table.signature td { margin-left: 0; width: 300px; }
+	table.signature td,th { margin-left: 0; padding-left: 0; width: 300px; }
+	p,li,td,th,span { font-size: 14pt; }
+	.mainContent { line-height: 3px; }
+	.topCopy { font-size: 10pt; line-height: 1px !important; }
 </style>
-<h1>Moorfields Eye Hospital NHS Foundation Trust</h1>
 <h2>Consent form 3</h2>
 <h2><?php echo $this->patient->fullName?>, Hospital no: <?php echo $this->patient->hos_num?></h2>
 <h3>Patient/parental agreement to investigation or treatment (procedures where consciousness not impaired)</h3>
 <p>
-	<strong>Name of procedure</strong> (include brief explanation if medical term not clear)
-</p>
-<p>
-	.....................................................................................................................................................................
+	<strong>Procedure(s):</strong>
+	<?php foreach ($elements['Element_OphTrConsent_Procedure']->procedures as $i => $procedure) {
+		if ($i >0) echo ', ';
+		echo $procedure->term;
+	}?>
 </p>
 <p>
 	<strong>Statement of health professional</strong> (to be filled in by health professional with appropriate knowledge of proposed procedure, as specified in consent policy)
 </p>
 <p>
 	<strong>I have explained the procedure to the patient/parent. In particular, I have explained:</strong>
-</p>
-<p><strong>The intended benefits:</strong></p>
-<p>
+	<br/>
+	<strong>The intended benefits:</strong>
 	<?php echo $elements['Element_OphTrConsent_BenefitsAndRisks']->benefits?>
-</p>
-<p><strong>Serious, frequently occurring or unavoidable risks:</strong></p>
-<p>
+	<br/>
+	<strong>Serious, frequently occurring or unavoidable risks:</strong>
 	<?php echo $elements['Element_OphTrConsent_BenefitsAndRisks']->risks?>
 </p>
 <?php if (!empty($elements['Element_OphTrConsent_Procedure']->additional_procedures)) {?>
-	<p>Any extra procedures which may become necessary during the procedure(s)</p>
+	<p>Any extra procedures which may become necessary during the procedure(s):</p>
 	<ul>
 		<?php foreach ($elements['Element_OphTrConsent_Procedure']->additional_procedures as $proc) {?>
 			<li><?php echo $proc->term?></li>
@@ -40,19 +42,17 @@
 <p>
 	I have also discussed what the procedure is likely to involve, the benefits and risks of any available alternative treatments (including no treatment) and any particular concerns of those involved.
 </p>
-<?php if ($elements['Element_OphTrConsent_Other']->information) {?>
-	<p>
-		An informational leaflet has been provided.
-	</p>
-<?php }?>
+<p>
+	[<?php if ($elements['Element_OphTrConsent_Other']->information) {?>x<?php }else{?>&nbsp;&nbsp;<?php }?>] An informational leaflet has been provided.
+</p>
 <table>
 	<tr>
-		<td>Signed:..............................................................</td>
-		<td>Date:..........................................................</td>
+		<td>Signed:...................................................</td>
+		<td>Date:.................................................</td>
 	</tr>
 	<tr>
-		<td>Name (PRINT):................................................</td>
-		<td>Job title:.....................................................</td>
+		<td>Name (PRINT):......................................</td>
+		<td>Job title:............................................</td>
 	</tr>
 </table>
 <br pagebreak="true"/>
@@ -63,8 +63,8 @@
 	</p>
 	<table>
 		<tr>
-			<td>Signed:..............................................................</td>
-			<td>Date:..........................................................</td>
+			<td>Signed:...............................................</td>
+			<td>Date:...............................................</td>
 		</tr>
 		<tr>
 			<td colspan="2">Name: <?php echo $elements['Element_OphTrConsent_Other']->interpreter_name?></td>
@@ -78,12 +78,12 @@
 </p>
 <table>
 	<tr>
-		<td>Signed:..............................................................</td>
-		<td>Date:..........................................................</td>
+		<td>Signed:.............................................</td>
+		<td>Date:...................................................</td>
 	</tr>
 	<tr>
-		<td>Name (PRINT):................................................</td>
-		<td>Relationship to patient:...............................................</td>
+		<td>Name (PRINT):.................................</td>
+		<td>Relationship to patient:.......................</td>
 	</tr>
 </table>
 <div class="spacer"></div>
@@ -92,12 +92,12 @@
 </p>
 <table>
 	<tr>
-		<td>Signed:..............................................................</td>
-		<td>Date:..........................................................</td>
+		<td>Signed:...................................................</td>
+		<td>Date:.................................................</td>
 	</tr>
 	<tr>
-		<td>Name (PRINT):................................................</td>
-		<td>Job title:......................................................</td>
+		<td>Name (PRINT):......................................</td>
+		<td>Job title:............................................</td>
 	</tr>
 </table>
 <div class="spacer"></div>
