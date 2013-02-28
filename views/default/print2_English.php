85fe717036d517be99fc141e7fe749275558e6ec
Mark <m@rkw.me>
2013-02-28 13:12:42 +0000

    [OE-2684] added the check to the anaesthetic leaflet checkbox on the printout when checked on the create form.

--- views/default/print2_English.php
+++ views/default/print2_English.php
@@ -157,7 +157,7 @@
 </p>
 <p>
 	[<?php if ($elements['Element_OphTrConsent_Other']->information) {?>x<?php }else{?>&nbsp;&nbsp;<?php }?>] An informational leaflet has been provided.<br/>
-	[&nbsp;&nbsp;] "Anaesthesia at Moorfields Eye Hospital" leaflet has been provided<br/>
+	[<?php if ($elements['Element_OphTrConsent_Other']->anaesthetic_leaflet) {?>x<?php }else{?>&nbsp;&nbsp;<?php }?>] "Anaesthesia at Moorfields Eye Hospital" leaflet has been provided<br/>
 	<strong>This procedure will involve:</strong>
 	[<?php if ($elements['Element_OphTrConsent_Procedure']->anaesthetic_type->name == 'GA') {?>x<?php }else{?>&nbsp;&nbsp;<?php }?>] general and/or regional anaesthesia<br/>[<?php if (in_array($elements['Element_OphTrConsent_Procedure']->anaesthetic_type->name,array('Topical','LAC','LA','LAS'))) {?>x<?php }else{?>&nbsp;&nbsp;<?php }?>] local anaesthesia&nbsp;&nbsp;[<?php if ($elements['Element_OphTrConsent_Procedure']->anaesthetic_type->name == 'LAS') {?>x<?php }else{?>&nbsp;&nbsp;<?php }?>] sedation
 </p>
