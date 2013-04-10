b36acebbd040cab30d483feac0a2ec4d37e18263
Mark <m@rkw.me>
2013-04-10 15:05:02 +0100

    [OE-2789] moved the print buttons into the sticky header.

--- views/default/view.php
+++ views/default/view.php
@@ -22,16 +22,16 @@
 ?>
 <h3 class="withEventIcon"><?php  echo $this->event_type->name ?></h3>
 
+<?php
+$this->event_actions[] = EventAction::button('Print', 'print', array('colour' => 'blue'));
+$this->event_actions[] = EventAction::button('Print for visually impaired', 'print_va', array('colour' => 'blue'));
+$this->renderPartial('//patient/event_actions');
+?>
+
 <div>
 	<?php  $this->renderDefaultElements($this->action->id); ?>
 	<?php  $this->renderOptionalElements($this->action->id); ?>
 	<div class="cleartall"></div>
-</div>
-
-<div class="form_button">
-	<img class="loader" style="display: none;" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." />&nbsp;
-	<button type="submit" class="classy blue venti" id="et_print" name="print"><span class="button-span button-span-blue">Print</span></button>
-	<button type="submit" class="classy blue venti" id="et_print_va" name="print"><span class="button-span button-span-blue">Print for visually impaired</span></button>
 </div>
 
 <iframe id="print_iframe" name="print_iframe" style="display: none;"></iframe>
