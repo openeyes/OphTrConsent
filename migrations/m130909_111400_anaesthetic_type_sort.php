c69c33881be7928c1309d95cbab86d4679a04d07
Jamie Neil <jamie.neil@openeyes.org.uk>
2013-09-09 10:42:56 +0000

    [OE-3393] Fix anaesthetic sort migration

--- migrations/m130909_111400_anaesthetic_type_sort.php
+++ migrations/m130909_111400_anaesthetic_type_sort.php
@@ -4,8 +4,10 @@ class m130909_111400_anaesthetic_type_sort extends OEMigration
 {
 	public function up()
 	{
+		$element_type_id = ElementType::model()->find('class_name = ?',array('Element_OphTrConsent_Procedure'))->id;
+		$this->delete('element_type_anaesthetic_type','element_type_id = ?',array($element_type_id));
 		$migrations_path = dirname(__FILE__);
-		$this->initialiseData($migrations_path, 'code');
+		$this->initialiseData($migrations_path);
 	}
 
 	public function down()
