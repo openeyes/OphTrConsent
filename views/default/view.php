33c7446183316398ac4b11f0075f8e7af192b05b
Mark <m@rkw.me>
2013-09-11 16:15:02 +0100

    updated to the new module style

--- views/default/view.php
+++ views/default/view.php
@@ -3,6 +3,13 @@
 ?>
 <h3 class="withEventIcon" style="background:transparent url(<?php echo $this->assetPath?>/img/medium.png) center left no-repeat;"><?php  echo $this->event_type->name ?></h3>
 
+<?php
+	if ($this->canPrint()) {
+		$this->event_actions[] = EventAction::button('Print', 'print');
+	}
+	$this->renderPartial('//patient/event_actions');
+?>
+
 <div>
 	<?php  $this->renderDefaultElements($this->action->id); ?>
 	<?php  $this->renderOptionalElements($this->action->id); ?>
@@ -12,7 +19,6 @@
 <div class="form_button">
 	Print language: <?php echo CHtml::dropDownList('printLanguage',12,CHtml::listData(Language::model()->findAll(),'id','name'))?>
 	<img class="loader" style="display: none;" src="<?php echo Yii::app()->createUrl('img/ajax-loader.gif')?>" alt="loading..." />&nbsp;
-	<button type="submit" class="classy blue venti" id="et_print" name="print"><span class="button-span button-span-blue">Print</span></button>
 </div>
 
 <iframe id="print_iframe" name="print_iframe" style="display: none;"></iframe>
