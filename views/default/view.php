24be5d4b17d82036808462a245fc10df1ebc67ce
Richard Willis <willis.rh@gmail.com>
2014-01-10 09:33:43 +0000

    Removed event title (this is now set in the event_container view in core)

--- views/default/view.php
+++ views/default/view.php
@@ -22,8 +22,6 @@ if ($this->checkPrintAccess()) {
 }
 ?>
 <?php $this->beginContent('//patient/event_container');?>
-	
-	<h2 class="event-title"><?php echo $this->event_type->name ?></h2>
 
 	<?php  $this->renderOpenElements($this->action->id); ?>
 	<?php  $this->renderOptionalElements($this->action->id); ?>
