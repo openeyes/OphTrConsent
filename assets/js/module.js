400829c48ceb263327116e426f00b55c6be45cde
Mark <m@rkw.me>
2014-01-17 14:42:34 +0000

    [OEM-109] cancel delete bugfix

--- assets/js/module.js
+++ assets/js/module.js
@@ -36,14 +36,7 @@ $(document).ready(function() {
 
 	handleButton($('#et_deleteevent'));
 
-	handleButton($('#et_canceldelete'),function(e) {
-		e.preventDefault();
-		if (m = window.location.href.match(/\/delete\/[0-9]+/)) {
-			window.location.href = window.location.href.replace('/delete/','/view/');
-		} else {
-			window.location.href = baseUrl+'/patient/episodes/'+OE_patient_id;
-		}
-	});
+	handleButton($('#et_canceldelete'));
 
 	$('select.populate_textarea').unbind('change').change(function() {
 		if ($(this).val() != '') {
