48df9bc033b18e5ca38c6f7245e4e6a6756bda18
Mark Wadham <m@rkw.me>
2014-10-23 08:44:58 +0000

    bugfix

--- views/default/select_event.php
+++ views/default/select_event.php
@@ -74,7 +74,7 @@
 											<span class="large-4 column">
 												<?php foreach ($operation->procedures as $i => $procedure) {
 													if ($i >0) { echo "<br/>"; }
-													echo $booking->operation->eye->name . ' ' . $procedure->term;
+													echo $operation->eye->name . ' ' . $procedure->term;
 												}?>
 											</span>
 										</span>
